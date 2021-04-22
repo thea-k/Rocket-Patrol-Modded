f4db07afb83d8c05fcc6f202e4444ba431693f88
thea-k <theaknasiak@gmail.com>
2021-04-21 18:58:07 -0700

    Git being weird

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * Name: Thea Knasiak
  * Project: Modded Rocket Patrol Assignment
@@ -17,18 +16,3 @@ let borderUISize = game.config.height / 15; //width of the border will be 1/15 t
 let borderPadding = borderUISize / 3; //width of the padded will be 1/3 the borderUISize
 
 let keyLeft, keyRight, keyF, keyR;
-=======
-let config = {
-    type: Phaser.CANVAS,
-    width: 640,
-    height: 480,
-    scene: [Menu, Play],
-}
-
-let game = new Phaser.Game(config);
-
-let borderUISize = game.config.height / 15; //width of the border will be 1/15 the height
-let borderPadding = borderUISize / 3; //width of the padded will be 1/3 the borderUISize
-
-let keyLeft, keyRight, keyF, keyR;
->>>>>>> ac9d5a75cf240b70e6717417ccd59a020b322bb0
